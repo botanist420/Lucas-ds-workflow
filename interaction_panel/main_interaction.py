304eba8cd6c9f986133ecb2a0044a8583676af94
botanist420 <lin36716@gmail.com>
2022-12-15 20:10:58 +0800

    add: data source and main interaction.py

---
+++ interaction_panel/main_interaction.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import os
+
+# example plot using tips
+tips = sns.load_dataset("tips")
+print("The head of tips dataset:")
+print(tips.head())
+
+
+
+def example_plot(data=tips, x="tip", y="total_bill", hue="sex", size="size", sizes=(10, 100), as_example=True):
+    
+    if as_example:
+        sns.scatterplot(data=data, x=x, y=y, hue=hue,
+                        size=size, sizes=sizes)
+        plt.show()
+        
+        
+        
+example_plot()
+    
